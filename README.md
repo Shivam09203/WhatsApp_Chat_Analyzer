11c00f1a0e1eba7007a6efcbc640c752755baf05
Shivam09203 <shivanu1825@gmail.com>
2024-10-13 20:10:33 +0530

    git commit

---
+++ README.md
@@ -0,0 +1,70 @@
+# WhatsChat
+
+## Overview
+
+**WhatsChat** is a _WhatsApp Chat Analyzer_ that provides fun insights about your chats. The project was created from scratch and uses various libraries such as Matplotlib, Urlextract, Pandas, Collections, Regex, and Datetime to extract and analyze chat data.
+
+## Technology Used
+
+- Python
+- Matplotlib
+- URLExtract
+- Pandas
+- Collections
+- Regex
+- Datetime
+- Streamlit
+- NLTK
+- Seaborn
+- Wordcloud
+
+## Features
+
+WhatsChat provides various features to analyze your chats, including:
+
+- _Top Statistics_: Total messages, words, media shared, and links shared.
+- _Monthly Timeline_: Provides activity details for each month.
+- _Monthly Activity_: Provides activity details for each month.
+- _Daily Timeline_: Provides activity details for each date.
+- _Daily Activity_: Provides activity details for each day of the week.
+- _Weekly Activity_: Shows the most and least active hours.
+- _Day-wise Activity_: Shows the most and least active days.
+- _Top Chatters_: Shows who chats the most in the group.
+- _Top Words Used_: Shows the most frequently used words.
+- _Word Cloud_: Generates a word cloud for the chat.
+- _Emoji Analysis_: Shows the most frequently used emojis.
+- _Messages Extractor_: Extracts messages for a specific date.
+- _Reply Time Analysis_: Shows how much time each person takes to reply to a message.
+- _Sentimental Analysis_: Performs sentiment analysis based on the text and emojis used in the messages.
+
+You can perform all these analyses on group chats as well as individual chats.
+
+## Demo
+
+You can see the demo of _WhatsChat_ by visiting the following link: [WhatsChat](https://shrudex-whatschat.streamlit.app/)
+
+## Usage
+
+To use WhatsChat, follow these steps:
+
+- Extract the chat data from WhatsApp by going to the chat you want to analyze, clicking on the three dots, selecting "More," selecting "Export Chat," and downloading the chat.
+- Visit the WhatsChat link and upload the chat data.
+- Enjoy the insights!
+
+_Note: Make sure the date format is 12-hour format._
+
+## Deployment
+
+WhatsChat is deployed on **Streamlit**, which is a platform for building and sharing data apps. **WhatsChat** uses _Streamlit_'s web application framework to provide an interactive and easy-to-use interface for analyzing your chat data.
+
+## Contributions
+
+Contributions are always welcome! If you want to contribute to this project, feel free to fork it, make changes, and submit a pull request.
+
+## Contact
+
+If you have any questions or suggestions, feel free to contact us at shivamgangal7@gmail.com.
+
+## Privacy
+
+**WhatsChat** doesn't store any data. All the analyses are performed on the uploaded chat data and are not stored anywhere. So you can be confident that your privacy is _protected_.
---
+++ app.py
@@ -0,0 +1,304 @@
+import streamlit as st
+import preprocessor, helper
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import emoji
+import seaborn as sns
+import plotly.express as px
+
+
+st.set_page_config(page_title="Whatsapp Chat Analyzer", layout="wide")
+
+st.title("WhatsChat🔎")
+st.write("*Made by Shivam Gangal!👨🏻‍💻*")
+
+st.sidebar.title("WhatsApp Chat Analyzer")
+uploadedFile = st.sidebar.file_uploader("Choose a File🗃️")
+if uploadedFile is not None:
+    bytesData = uploadedFile.getvalue()
+    finalData = bytesData.decode("utf-8")
+    dataFrame = preprocessor.preprocess(finalData)
+    #st.title("WhatsApp Chat Data")
+    #st.dataframe(dataFrame.head())
+
+    # fetch unique users
+    userList = dataFrame["user"].unique().tolist()
+    if "default" in userList:
+        userList.remove("default")
+    userList.sort()
+    userList.insert(0, "Overall")
+    selectedUser = st.sidebar.selectbox("Show Analysis🤔 WRT", userList)
+
+    #if st.sidebar.button("Show Analysis🔢"):
+    if (True):
+        #top statistics
+        numMessages, numWords, numMedia, numURL = helper.fetchStats(
+            selectedUser, dataFrame)
+        st.title("Top Statistics📈")
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.header("Total Messages🤳🏻")
+            st.header(numMessages)
+        with col2:
+            st.header("Total Words💭")
+            st.header(numWords)
+        with col3:
+            st.header("Media Shared🎥")
+            st.header(numMedia)
+        with col4:
+            st.header("Links Shared🔗")
+            st.header(numURL)
+
+        #monthly timeline
+        st.title("Monthly Timeline⌚")
+        timeline = helper.monthlyTimeline(selectedUser, dataFrame)
+        plt.style.use('dark_background')
+        plt.figure(figsize=(12, 3))
+        plt.plot(timeline['time'], timeline['message'])
+        plt.xticks(rotation='vertical')
+        plt.xlabel('Month', color='yellow')
+        plt.ylabel('Message Count', color='yellow')
+        st.pyplot(plt)
+        
+        
+        #monthly activity
+        st.title("Monthly Activity📊")
+        col1, col2 = st.columns(2)
+        monthActivitySeries, monthActivity = helper.monthActivity(selectedUser, dataFrame)
+        monthActivity = monthActivity.sort_values('message')
+        month = monthActivity['monthName']
+        messages = monthActivity['message']
+        
+        with col2:
+            fig, ax = plt.subplots(figsize=(8, 6))
+            ax.pie(messages.head(), labels=month.head().tolist(), autopct='%1.1f%%', colors=plt.cm.Dark2.colors)
+            ax.axis('equal')
+            plt.style.use('dark_background')
+            st.pyplot(fig)
+            
+            
+        with col1:
+            fig, ax = plt.subplots(figsize=(8, 6))
+            ax.bar(month, messages)
+            ax.set_xlabel('Month of the Year', color="yellow")
+            ax.set_ylabel('Number of Messages', color='yellow')
+            plt.xticks(rotation='vertical')
+            plt.style.use('dark_background')
+            st.pyplot(fig)
+
+
+        #daily timeline
+        st.title("Daily Timeline📅")
+        dailyTimeline = helper.dailyTimeline(selectedUser, dataFrame)
+        plt.style.use('dark_background')
+        plt.figure(figsize=(14, 3))
+        plt.plot(dailyTimeline['onlyDate'], dailyTimeline['message'])
+        plt.xticks(rotation='vertical')
+        plt.xlabel('Date', color='yellow')
+        plt.ylabel('Message Count', color='yellow')
+        st.pyplot(plt)
+
+        #daily activity
+        st.title("Daily Activity📊")
+        col1, col2 = st.columns(2)
+        weekActivitySeries, weekActivity = helper.weekActivity(selectedUser, dataFrame)
+        weekActivity = weekActivity.sort_values('message')
+        days = weekActivity['dayName']
+        messages = weekActivity['message']
+        
+        with col2:
+            fig, ax = plt.subplots(figsize=(8, 6))
+            ax.pie(messages, labels=days.tolist(), autopct='%1.1f%%', colors=plt.cm.Dark2.colors)
+            ax.axis('equal')
+            plt.style.use('dark_background')
+            st.pyplot(fig)
+            
+            
+        with col1:
+            fig, ax = plt.subplots(figsize=(8, 6))
+            ax.bar(days, messages)
+            ax.set_xlabel('Day of the Week', color="yellow")
+            ax.set_ylabel('Number of Messages', color='yellow')
+            plt.style.use('dark_background')
+            st.pyplot(fig)
+        
+        
+        #weekly activity
+        st.title("Weekly Activity by Time Period📲")
+        activity = helper.activity(selectedUser, dataFrame)
+        fig, ax = plt.subplots(figsize=(10, 2.5))
+        ax = sns.heatmap(activity)
+        ax.set_xlabel('Time Period', color='yellow')
+        ax.set_ylabel('Name of the Day', color='yellow')
+        plt.style.use('dark_background')
+        st.pyplot(fig)
+        
+        
+        #day-wise activity
+        st.title("Day-wise Activity🗓️")
+        h1, h2 = helper.hourActivity(selectedUser, dataFrame)
+        tabs = st.multiselect("Select day(s) to display",['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+        #tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+        days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        
+        for day in tabs:
+            day_data = h2[h2['dayName'] == day]
+            plot_placeholder = st.empty()
+            with plot_placeholder:
+                fig, axs = plt.subplots(figsize=(12, 3))
+                axs.plot(day_data['hour'], day_data['message'])
+                axs.set_title(day)
+                axs.set_xlabel('Hour of the Day', color='yellow')
+                axs.set_ylabel('Number of Messages', color='yellow')
+                axs.set_xticks(range(0, 24, 2))
+                axs.grid(True, alpha=0.3)
+                plt.tight_layout()
+                st.pyplot(fig)
+
+
+        #top chatters
+        if selectedUser == 'Overall':
+            st.title("Top Chatters🗣️")
+            topChatter, topChatterPercent = helper.mostBusy(dataFrame)
+            col1, col2 = st.columns(2)
+
+            with col1:
+                plt.style.use('dark_background')
+                name = topChatter.index
+                name = [emoji.emojize(n) for n in name]
+                count = topChatter.values
+                fig, ax = plt.subplots()
+                plt.xlabel('Name').set_color('yellow')
+                plt.ylabel('Messages Sent').set_color('yellow')
+                ax.bar(name, count, width=0.8)
+                plt.xticks(rotation='vertical')
+                ax.tick_params(axis='both', which='major', labelsize=8)
+
+                st.pyplot(fig)
+
+            with col2:
+                st.dataframe(topChatterPercent)
+
+   
+        #top words used
+        mostCommon = helper.mostCommon(selectedUser, dataFrame)
+        if (mostCommon.shape[0] != 0):
+            st.title("Top Words Used🥇")
+
+            fig, ax = plt.subplots()
+            plt.ylabel('Message').set_color('yellow')
+            plt.xlabel('Frequency').set_color('yellow')
+            ax.barh(mostCommon['Message'], mostCommon['Frequency'])
+            plt.xticks(rotation="vertical")
+            st.pyplot(fig)
+                
+        
+        #wordcloud
+        df_wc = helper.wordCloud(selectedUser, dataFrame)
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            st.title("Wordcloud🌬️")   
+            fig, ax = plt.subplots()
+            ax.imshow(df_wc)
+            st.pyplot(fig)
+            
+        with col2:
+            st.title("Top Words' Count🗣️")
+            st.dataframe(mostCommon)
+            
+        
+
+        # emoji analysis
+        emoji_df = helper.mostEmoji(selectedUser, dataFrame)
+        if (emoji_df.shape[0] != 0):
+            st.title("Emoji Analysis😳")
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                st.dataframe(emoji_df)
+            with col2:
+                fig, ax = plt.subplots()
+                color = ['#FFC107', '#2196F3', '#4CAF50', '#F44336', '#9C27B0']
+
+                ax.pie(emoji_df['Count'].head(), labels=emoji_df['Emoji'].head().tolist(), autopct="%0.2f", colors=color)
+                ax.set_title("Emoji Distribution", color='yellow')
+                fig.set_facecolor('#121212')
+                st.pyplot(fig)
+        
+        #message extractor
+        st.title("Messages Extractor🪓")
+        inputDate = st.text_input("Enter date in format : 19-08-2003")
+        messageExtract = helper.messageExtractor(selectedUser, dataFrame, inputDate)
+        if st.button("Extract"):
+            if messageExtract is not None and messageExtract.shape[0]>0:
+                st.dataframe(messageExtract, width=1400)
+            else:
+                st.write("No conversation(s) on", inputDate)  
+
+        #reply time analysis
+        st.title("Reply Time Analysis⏩")
+        timeDifference, timeSelected = helper.replyTime(selectedUser, dataFrame)
+        if (selectedUser!='Overall'):
+            st.write("Average Reply Time by", selectedUser, "is", timeSelected)
+        else:
+            col1, col2 = st.columns(2)
+            with col1:
+                fig, ax = plt.subplots(figsize=(8, 6))
+                ax.bar(timeDifference['user'], timeDifference['replyTime'].dt.seconds)
+                ax.set_xlabel('Participant', color='yellow')
+                ax.set_ylabel('Average Reply Time (Seconds)', color='yellow')
+                ax.set_title('')
+                st.pyplot(plt)
+            with col2:
+                fig, ax = plt.subplots(figsize=(8, 6))
+                ax.pie(timeDifference['replyTime'].head(), labels=timeDifference['user'].head().tolist(), autopct='%1.1f%%', colors=plt.cm.Dark2.colors)
+                ax.axis('equal')
+                plt.style.use('dark_background')
+                ax.set_title('')
+                st.pyplot(fig)
+                
+        
+        # User Sentiment Analysis
+        st.title("Sentiment Analysis")
+
+        # Perform sentiment analysis for the selected user or overall
+        user_sentiments = helper.analyze_sentiment(selectedUser, dataFrame)
+
+        # If the selected user is 'Overall', display sentiment for all users
+        if selectedUser == 'Overall':
+            overall_sentiment = {key: sum([user[key] for user in user_sentiments.values()])/len(user_sentiments) for key in ['pos', 'neu', 'neg', 'compound']}
+            st.subheader("Overall")
+
+            # Create a horizontal bar chart for the overall sentiment scores
+            fig, ax = plt.subplots()
+            ax.barh(list(overall_sentiment.keys()), list(overall_sentiment.values()), color=['green' if v >= 0 else 'red' for v in overall_sentiment.values()])
+            ax.set_xlabel('Score', color='yellow')
+            ax.set_title('Overall sentiment scores', color='yellow')
+            st.pyplot(fig)
+        else:
+            # If the selected user exists in the sentiment analysis results
+            if selectedUser in user_sentiments:
+                sentiment_scores = user_sentiments[selectedUser]
+
+                st.subheader(f"User: {selectedUser}")
+
+                # Create a horizontal bar chart for the sentiment scores
+                fig, ax = plt.subplots()
+                ax.barh(list(sentiment_scores.keys()), list(sentiment_scores.values()), color=['yellow' if v >= 0 else 'red' for v in sentiment_scores.values()])
+                ax.set_xlabel('Score')
+                ax.set_title(f'Sentiment scores for {selectedUser}')
+                st.pyplot(fig)
+            else:
+                st.write(f"No sentiment analysis results for {selectedUser}")
+
+        ## Final results for the sentiment analysis
+
+        # Perform sentiment analysis for the selected user or overall
+        user_sentiments = helper.analyze_sentiment(selectedUser, dataFrame)
+
+        # Display the sentiment score
+        sentiment = helper.sentiment_score(user_sentiments, selectedUser)
+        st.subheader(f"The sentiment of {selectedUser} is {sentiment}")
